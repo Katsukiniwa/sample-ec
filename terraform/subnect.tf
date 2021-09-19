1074dc3ab403315275a27961a93029b309e72340
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-09-19 19:03:23 +0900

    craete vpc and subnet

---
+++ terraform/subnect.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "public" {
+  for_each = local.availability_zones
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(local.vpc_cidr, 8, local.az_conf[each.key].index)
+
+  tags = {
+    Name = "${local.name}-public-${local.az_conf[each.key].short_name}"
+  }
+}
