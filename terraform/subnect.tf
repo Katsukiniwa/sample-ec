96f20c992d2b9683346c2c5ba923cae1b1b2fb5f
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-09-19 20:55:52 +0900

    create ecs subnet

--- terraform/subnect.tf
+++ terraform/subnect.tf
@@ -9,3 +9,15 @@ resource "aws_subnet" "public" {
     Name = "${local.name}-public-${local.az_conf[each.key].short_name}"
   }
 }
+
+resource "aws_subnet" "ecs" {
+  for_each = local.availability_zones
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = each.key
+  cidr_block        = cidrsubnet(local.vpc_cidr, 8, length(aws_subnet.public) + local.az_conf[each.key].index)
+
+  tags = {
+    Name = "${local.name}-ecs-${local.az_conf[each.key].short_name}"
+  }
+}
