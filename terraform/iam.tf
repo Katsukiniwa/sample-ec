299abf96cb42700bfa9e49fad4f45c5e182bbecb
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-15 22:56:51 +0900

    create iam module

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,15 +1,15 @@
 # ポリシードキュメント
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
-    effect = "Allow"
-    actions = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
     resources = ["*"]
   }
 }
 
 # IAMポリシー
 resource "aws_iam_policy" "example" {
-  name = "example"
+  name   = "example"
   policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
@@ -17,9 +17,9 @@ resource "aws_iam_policy" "example" {
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
-    
+
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["ec2.amazonaws.com"] # EC2にのみ関連付けできる
     }
   }
@@ -27,12 +27,12 @@ data "aws_iam_policy_document" "ec2_assume_role" {
 
 # IAMロール
 resource "aws_iam_role" "example" {
-  name = "example"
+  name               = "example"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
 
 # IAMポリシーのアタッチ
 resource "aws_iam_role_policy_attachment" "example" {
-  role = aws_iam_policy.example.name
+  role       = aws_iam_policy.example.name
   policy_arn = aws_iam_policy.example.arn
 }
