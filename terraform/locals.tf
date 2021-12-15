c2b8f7ed5446181656e1e728189c533380e9456a
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-15 22:54:00 +0900

    run terraform fmt

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -10,7 +10,7 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
   region     = data.aws_region.current.name
 
-  name   = "katsukiniwa"
+  name = "katsukiniwa"
   # domain = "katsukiniwa.dev"
 
   availability_zones = toset(data.aws_availability_zones.available.names)
