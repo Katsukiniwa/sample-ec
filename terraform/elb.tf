348c4c56ec690bae75242c64852bb1f3d4e4997a
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-30 13:02:17 +0900

    create security group for elb

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -25,3 +25,29 @@ resource "aws_lb" "example" {
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
+
+# セキュリティグループの設定
+
+module "http_sg" {
+  source = "./security_group"
+  name = "http_sg"
+  vpc_id = aws_vpc.example.id
+  port = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "https_sg" {
+  source = "./security_group"
+  name = "https_sg"
+  vpc_id = aws_vpc.example.id
+  port = 443
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "http_redirect_sg" {
+  source = "./security_group"
+  name = "http_redirect_sg"
+  vpc_id = aws_vpc.example.id
+  port = 8080
+  cidr_blocks = ["0.0.0.0/0"]
+}
