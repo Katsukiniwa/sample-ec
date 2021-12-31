d4637dcada7560d54842dfd79b5c7b7fa7ad85de
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-31 22:15:19 +0900

    fix security group

--- terraform/security_group/security_group.tf
+++ terraform/security_group/security_group.tf
@@ -17,7 +17,7 @@ resource "aws_security_group_rule" "ingress" {
   type              = "ingress"
   from_port         = var.port
   to_port           = var.port
-  protocol          = var.port
+  protocol          = "tcp"
   cidr_blocks       = var.cidr_blocks
   security_group_id = aws_security_group.default.id
 }
