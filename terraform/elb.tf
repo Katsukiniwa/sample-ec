954ee65388d645d63c372fe2b6126840926353cd
Katsukiniwa <katsukiniwa0217@gmail.com>
2022-01-03 17:25:24 +0900

    create target group

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -103,3 +103,27 @@ resource "alb_elb_listener" "redirect_to_https" {
     status_code = "HTTP_301"
   }
 }
+
+# ターゲットグループ
+
+resource "aws_lb_target_group" "example" {
+  name = "example"
+  target_type = "ip"
+  vpc_id = aws_vpc.example.id
+  port = "80"
+  protocol = "HTTP"
+  deregistration_delay = 300
+
+  health_check {
+    path = "/"
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+    matcher = 200
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+
+  depends_on = [aws_lb.example]
+}
